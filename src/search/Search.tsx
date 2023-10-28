5029f4244409f5a71bc200e5985ac2b6e6666ba9
eordan <pistsouandrey@gmail.com>
2023-10-28 09:49:54 +0300

    feat: add react component to handle search and submit

--- src/search/Search.tsx
+++ src/search/Search.tsx
@@ -1,33 +1,56 @@
 import './Search.css';
+import React from 'react';
 
-function Search() {
-  let placeHolder: string | null = '';
-  if (localStorage.getItem('searchItem') !== null) {
-    placeHolder = localStorage.getItem('searchItem') as string;
+class Search extends React.Component<object, { searchTerm: string }> {
+  constructor(props: object) {
+    super(props);
+    this.state = {
+      searchTerm: '', // Define searchTerm in the initial state
+    };
   }
 
-  console.log(placeHolder);
+  componentDidMount() {
+    // Load the saved search term from local storage on component mount
+    const savedTerm = localStorage.getItem('searchItem');
+    if (savedTerm) {
+      this.setState({ searchTerm: savedTerm });
+    }
+  }
 
-  return (
-    <div className="search-section">
-      <form action="/action_page.php">
-        <input
-          type="text"
-          name="search"
-          placeholder={placeHolder}
-          className="search-bar"
-        />
-        <button
-          onClick={() => localStorage.setItem('searchItem', 'Search Term2')}
-          type="submit"
-          value="Search"
-          className="submit-button"
-        >
-          Search
-        </button>
-      </form>
-    </div>
-  );
+  handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    // Update the search term in state when the input changes
+    this.setState({ searchTerm: event.target.value });
+  };
+
+  handleSearch = () => {
+    // Save the search term to local storage when the button is clicked
+    localStorage.setItem('searchItem', this.state.searchTerm);
+  };
+
+  handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault(); // Prevent the form from submitting
+    this.handleSearch();
+  };
+
+  render() {
+    return (
+      <div className="search-section">
+        <form onSubmit={this.handleSubmit} action="/action_page.php">
+          <input
+            type="text"
+            name="search"
+            placeholder={this.state.searchTerm}
+            className="search-bar"
+            onChange={this.handleInputChange}
+            value={this.state.searchTerm}
+          />
+          <button type="submit" value="Search" className="submit-button">
+            Search
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default Search;
